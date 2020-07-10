c8913e5bc70ded800d1b1b7b98a84b4d4f4ac63b
zenieldanaku <danielrossyamitrano@gmail.com>
2020-07-10 18:31:28 -0300

    Version 0.2.1
    * update_songlist.py ahora puede eliminar las entradas que aparecen
      en la sonlist pero que no existen en realidad. También incluye la
      capacidad de refrescar los weights o dejarlos como están.

--- update_songlist.py
+++ update_songlist.py
@@ -1,15 +1,43 @@
 from util import guardar_json, abrir_json
-from os import walk
+from os import walk, path
 
+filename = 'songlist.json'
 config = abrir_json('config.json')
 root = config['root']
+songlist = abrir_json(filename)
+
+refresh_weights = True
 
 data = []
+i = -1
 for basepath, _, files in walk(root):
     ruta = '/'.join(basepath.split('\\'))
     for file in files:
         if file.endswith('.mp3'):
-            data.append({'song': '/'.join([ruta, file]), 'weight': 1})
+            d = {'song': '/'.join([ruta, file])}
+            i += 1
+            try:
+                song = songlist[i]['song']
+            except IndexError as error:
+                # no estoy seguro de que esta sea la solución, porque el algoritmo solo se da cuenta de que sobran
+                # indices una vez que terminó de recorrer la lista, por lo que los pesos pueden quedar corridos.
+                # una solución podría ser que en lugar de sonlist, sea un songdict, con la ruta de las canciones
+                # como keys, pero eso alteraría el orden. Aunque de todos modos el orden es aleatorio.
+                print(error)
+            if refresh_weights:
+                d.update({'weight': 1})
+            else:
+                d.update({'weight': songlist[i]['weight']})
 
-filename = 'songlist.json'
+            data.append(d)
+
+flagged = []
+for i, item in enumerate(songlist):
+    song = item['song']
+    if not path.exists(song):
+        flagged.append(i)
+
+for idx in flagged:
+    del songlist[idx]
+
 guardar_json(data, filename)
